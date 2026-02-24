48f573c22579dd3b0b913dd640fae505c1341bd9
추석보 seokbo.choo <seokbo.choo@kurlycorp.com>
2026-02-24 09:03:36 +0900

    first commit

---
+++ src/test/java/org/example/awsplayground/AwsPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.awsplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
